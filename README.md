b70fd32581f47e1406323e3ef679187ea1bef30d
Donatas Abraitis <donatas@vinted.com>
2016-02-17 19:18:33 +0200

    First release

---
+++ README.md
@@ -0,0 +1,7 @@
+## Output example
+
+```
+curl "http://\[2001:dead::5\]:9119/metrics"
+node_openvz_laverage{hostname="php55.1455305656", ip="2001:feed:beef:1::3"} 0.0
+node_openvz_laverage{hostname="mariadb.1455712038", ip="2001:feed:beef:1::2"} 0.0
+```
---
+++ openvz_exporter.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'json'
+
+set :bind, '::0'
+set :port, 9119
+
+class OpenVZPrometheus
+  def vzlist
+    JSON.parse(`vzlist -a -o hostname,ip,laverage -j`)
+  end
+
+  def to_prometheus(hostname, ip, value)
+    "node_openvz_laverage{hostname=\"#{hostname}\", ip=\"#{ip}\"} #{value}\n"
+  end
+
+  def metrics
+    vzlist.map do |vz|
+      to_prometheus(vz['hostname'], vz['ip'].first, vz['laverage'].first)
+    end
+  end
+end
+
+get '/metrics' do
+  content_type 'text/plain'
+  OpenVZPrometheus.new.metrics
+end
