977d704d26490186a5616f9e9abb67a6738963bc
Donatas Abraitis <donatas@vinted.com>
2016-02-18 10:11:56 +0200

    Add python version

---
+++ openvz_exporter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import subprocess
+import json
+from flask import Flask
+app = Flask(__name__)
+
+def vzlist():
+    command = "vzlist -a -o hostname,ip,laverage -j"
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    return p.communicate()[0]
+
+def to_prometheus(hostname, ip, value):
+    return 'node_openvz_laverage{hostname="%s", ip="%s"} %s' % (hostname, ip, value)
+
+@app.route("/metrics")
+def metrics():
+    output = []
+    for vz in json.loads(vzlist()):
+        output.append(to_prometheus(vz['hostname'], vz['ip'][0], vz['laverage'][0]))
+
+    return "\n".join(output), 200, {'Content-type': 'text/plain'}
+
+if __name__ == "__main__":
+    app.run(host='::', port=9119)
